65be5312921813cff70dee20f69785ae0d44d6e0
@Amukayoung <amukayoung@gmail.com>
2023-05-10 23:43:04 +0300

    IMplemented removed method

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const populationData = {
 };
 d3.select("ul")
 	.selectAll("li")
-	.data(populationData.cities)
+	.data(populationData.cities, (data) => data)
 	.enter()
 	.append("li")
 	.text((data) => data);
@@ -29,9 +29,29 @@ setTimeout(() => {
 	populationData.addCity("Arua");
 	d3.select("ul")
 		.selectAll("li")
-		.data(populationData.cities)
+		.data(populationData.cities, (data) => data)
 		.enter()
 		.append("li")
 		.classed("added", true)
 		.text((data) => data);
 }, 2000);
+
+setTimeout(() => {
+	populationData.removeCity(0);
+	d3.select("ul")
+		.selectAll("li")
+		.data(populationData.cities, (data) => data)
+		.exit()
+		.classed("redundant", true)
+		.text((data) => data);
+}, 4000);
+
+setTimeout(() => {
+	populationData.updateCity(2, "Jinja");
+	d3.select("ul")
+		.selectAll("li")
+		.data(populationData.cities, (data) => data)
+		.exit()
+		.classed("updated", true)
+		.text((data) => data);
+}, 6000);
